7a306035c23f2c21f6d0746996696ffa59577b3b
ssghajar <ssghajar@gmail.com>
2020-07-26 17:14:38 +0430

    profile beautified

--- src/database/elasticsearch/services/profile.js
+++ src/database/elasticsearch/services/profile.js
@@ -2,7 +2,7 @@ import { Client } from '@elastic/elasticsearch';
 const esClient = new Client({ node: 'http://localhost:9200' });
 import { config } from '../../../config';
 const index = config.elasticsearch.indices.profile_index
-//--------------------------------------------------------------------------------------------
+//------------------------------------------GET PROFILE--------------------------------------------------
 async function getProfile(userId) {
     try {
         const { body } = await esClient.get({
@@ -27,5 +27,26 @@ async function getProfile(userId) {
         return error
     }
 }
-//--------------------------------------------------------------------------------------------
-export { getProfile, }
+//------------------------------------------UPDATE PROFILE----------------------------------------
+async function updateProfile(userId, username, password) {
+    try {
+        const { body } = await esClient.update({
+            index,
+            id: userId,
+            body: {
+                doc: {
+                    username: username.trim(),
+                    password: password.trim()
+                }
+            }
+        })
+        console.log("updateProfile", body)
+        let response = body.result && (body.result == 'updated' || body.result == 'noop') ? body.result : false
+        return response
+    } catch (error) {
+        console.error("getProfile-error", error)
+        throw error
+    }
+}
+//-----------------------------------------------------------------------------------------------
+export { getProfile, updateProfile }
--- src/routers/user/services/profile.js
+++ src/routers/user/services/profile.js
@@ -1,12 +1,6 @@
 "use strict";
 // ----------------------------------------------------------------
-import { config } from '../../../config';
-import { Client } from '@elastic/elasticsearch';
-const esClient = new Client({ node: 'http://localhost:9200' });
-import redis from 'redis';
-const redisClient = redis.createClient();
-import jwt from 'jsonwebtoken';
-import { getProfile } from '../../../database/elasticsearch/services/profile';
+import { getProfile, updateProfile } from '../../../database/elasticsearch/services/profile';
 import { tokenValidator } from '../../../database/redis/services/token_validator';
 // ----------------------------------------------------------------
 const prepare = (router, route) => {
@@ -57,54 +51,29 @@ const prepare = (router, route) => {
         let data = {
             error: false,
         }
-        if (authorization && agent && username && password) {
-            let authToken = authorization.slice(7, authorization.length)
-            try {
-                return jwt.verify(authToken, config.secret, (jwtErr, decoded) => {
-                    if (!(decoded && 'token' in decoded)) {
-                        console.error("jwtErr", jwtErr)
-                        return res.status(401).json(error)
-                    }
-                    redisClient.get(decoded.token, (err, reply) => {
-                        if (reply) {
-                            let agentIndex = decoded.token.search(agent)
-                            if (agentIndex != -1) {
-                                let userId = decoded.token.slice(0, agentIndex - 1)
-                                // elasticsearch update data with userId 
-                                const esUpdateRequest = esClient.update({
-                                    index: 'profile',
-                                    id: userId,
-                                    body: {
-                                        doc: {
-                                            username: username.trim(),
-                                            password: password.trim()
-                                        }
-                                    }
-                                })
-                                esUpdateRequest.then(({ body }) => {
-                                    console.log("body-update", body)
-                                    data.result = "profile updated"
-                                    return res.status(200).json(data)
-                                }).catch((err) => {
-                                    console.error("error-update", err)
-                                })
-                            } else {
-                                return res.status(401).json(error)
-                            }
-                        } else {
-                            return res.status(401).json(error)
-                        }
-                    });
-                });
-            } catch (err) {
-                console.error("catch-err", err)
-                return res.status(401).json(error)
+        tokenValidator(authorization, agent, userId => {
+            updateProfile(userId, username, password).then(response => {
+                if (response) {
+                    console.log("body-update", response)
+                    data.result = "profile updated"
+                    return res.status(200).json(data)
+                }
+            }).catch((err) => {
+                console.error("error-update", err)
+            })
+        }, err => {
+            switch (err) {
+                case 400:
+                    error.result = "Bad Request"
+                    return res.status(400).json(error)
+                case 401:
+                    console.error("catch-err", err)
+                    return res.status(401).json(error)
             }
-        }
-        else {
-            error.result = "Bad Request"
-            return res.status(400).json(error)
-        }
+        }).catch(err => {
+            console.error("catch-err", err)
+            return res.status(401).json(error)
+        })
     })
 }
 // ----------------------------------------------------------------
